30edafa0fa52775d9d47d72566efbe655844665b
Wang Panzhenzhuan <randy.wang@rock-chips.com>
2018-09-05 17:38:55 +0800

    CameraHal_V1.0.1: Distinguish camera3 profile for rk3326 and rk3126c
    Change-Id: Ia6029da3b2175da074fdc2533ef900ba8aaabf3f
    Signed-off-by: Wang Panzhenzhuan <randy.wang@rock-chips.com>

--- etc/camera_etc.mk
+++ etc/camera_etc.mk
@@ -1,9 +1,17 @@
 # prebuilt for config xml files in /vendor/etc/camera or /system/etc/camera
 CUR_PATH := $(TOP)/hardware/rockchip/camera/etc
+ifeq ($(filter box vr stbvr, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
 ifeq (1,$(strip $(shell expr $(PLATFORM_VERSION) \>= 8.0)))
+
 PRODUCT_COPY_FILES += \
+	$(CUR_PATH)/camera/camera3_profiles_$(TARGET_BOARD_PLATFORM).xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera3_profiles.xml \
 	$(call find-copy-subdir-files,*,$(CUR_PATH)/camera,$(TARGET_COPY_OUT_VENDOR)/etc/camera)
 else
 PRODUCT_COPY_FILES += \
+	$(CUR_PATH)/camera/camera3_profiles_$(TARGET_BOARD_PLATFORM).xml:$(TARGET_COPY_OUT_SYSTEM)/etc/camera/camera3_profiles.xml \
 	$(call find-copy-subdir-files,*,$(CUR_PATH)/camera,$(TARGET_COPY_OUT_SYSTEM)/etc/camera)
+
 endif
+endif
+
+
