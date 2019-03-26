d10de298e52b9020cf524a881ec35696c1d935d9
ZhongYichong <zyc@rock-chips.com>
2019-03-26 18:13:22 +0800

    fix wrong rk1608 fw path for version Android 8.0 or upper
    fw shoud be put to vendor/firmware, not vendor/etc/firmware/
    if version Android 8.0 is Android 8.0 or upper.
    add fw_rk1608_bypass.rkl for generic rk1608 test.
    
    Change-Id: I989c07c97eebeaa2cd853343f358ef317223b1cd
    Signed-off-by: ZhongYichong <zyc@rock-chips.com>

--- etc/camera_etc.mk
+++ etc/camera_etc.mk
@@ -10,7 +10,7 @@ ifeq (1,$(strip $(shell expr $(PLATFORM_VERSION) \>= 8.0)))
 
 PRODUCT_COPY_FILES += \
 	$(CUR_PATH)/camera/camera3_profiles_$(TARGET_BOARD_PLATFORM).xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera3_profiles.xml \
-	$(call find-copy-subdir-files,*,$(CUR_PATH)/firmware,$(TARGET_COPY_OUT_VENDOR)/etc/firmware) \
+	$(call find-copy-subdir-files,*,$(CUR_PATH)/firmware,$(TARGET_COPY_OUT_VENDOR)/firmware) \
 	$(call find-copy-subdir-files,*,$(CUR_PATH)/camera,$(TARGET_COPY_OUT_VENDOR)/etc/camera)
 else
 PRODUCT_COPY_FILES += \
